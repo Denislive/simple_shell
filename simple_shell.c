1d64ff3666602ca5f6c6895e0b5b28e65de1e0c6
Denislive <dennygachemi6@gmail.com>
2023-09-20 14:17:31 -0700

    Added simple_shell.c

--- simple_shell.c
+++ simple_shell.c
@@ -1,62 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
-#include <sys/wait.h>
 
-#define BUFFER_SIZE 1024
+/**
+ * main - Entry point for the simple shell program.
+ *
+ * Description: This program implements a simple shell that takes user
+ *              commands and executes them using execve.
+ *
+ * Return: Always returns EXIT_SUCCESS.
+ */
+int main(void) {
+    char *line = NULL;
+    size_t len = 0;
+    char *args[2];
 
-int main(void)
-{
-    char buffer[BUFFER_SIZE];
-    ssize_t bytes_read;
-    pid_t child_pid;
-    int status;
-
-    char *exec_args[] = {NULL, NULL};
-    char *exec_env[] = {NULL};
-
-    while (1)
-    {
-        printf("$ ");
-        bytes_read = read(STDIN_FILENO, buffer, BUFFER_SIZE);
-
-        if (bytes_read == -1)
-        {
-            perror("Error reading command");
-            exit(EXIT_FAILURE);
-        }
-
-        if (bytes_read == 0)
-        {
-            putchar('\n');
+    while (1) {
+        printf("#cisfun$ ");
+        if (getline(&line, &len, stdin) == -1) {
+            
+            printf("\n");
             break;
         }
 
-        buffer[bytes_read - 1] = '\0';
-
-        child_pid = fork();
-
-        if (child_pid == -1)
-        {
-            perror("Error forking process");
-            exit(EXIT_FAILURE);
+        if (line[strlen(line) - 1] == '\n') {
+            line[strlen(line) - 1] = '\0';
         }
 
-        if (child_pid == 0)
-        {
-            exec_args[0] = buffer;
+        args[0] = line;
+        args[1] = NULL;
 
-            if (execve(buffer, exec_args, exec_env) == -1)
-            {
-                perror("Error executing command");
-                exit(EXIT_FAILURE);
-            }
-        }
-        else
-        {
-            wait(&status);
+        if (execve(line, args, NULL) == -1) {
+            perror("Error");
         }
     }
 
-    return (0);
+    free(line);
+    return EXIT_SUCCESS;
 }
+
